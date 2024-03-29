69d5973a32f5e5e4735ef70965cb429fe3e084a7
unknown <muslihuddinsadi@gmail.com>
2024-03-29 19:10:10 +0700

    improve

--- admin/src/pages/home/index.js
+++ admin/src/pages/home/index.js
@@ -1,11 +1,13 @@
+import baffle from "baffle";
 import React, { useEffect, useState } from "react";
 import { Button, Card, Container } from "reactstrap";
 
-const jobs = ["Fullstack Developer", "Math modeller", "Android Developer"];
+const jobs = ["Web Developer", "Math modeller", "Android Developer"];
 
 const Home = () => {
   const [job, setJob] = useState(0);
 
+  //typing effect
   useEffect(() => {
     const interval = setInterval(() => {
       if(job !== jobs.length-1){
@@ -17,11 +19,22 @@ const Home = () => {
 
     return () => clearInterval(interval);
   }, [job]);
+
+  //glitch effect
+  useEffect(() => {
+    const text = baffle(document.querySelectorAll(".text-glitch"));
+    text.set({
+        characters: "█▓█ ▒░/▒░ █░▒▓/ █▒▒ ▓▒▓/█ ░█▒/ ▒▓░ █<░▒ ▓/░>",
+        speed: 100
+    });
+    text.start();
+    text.reveal(4000);
+  },[])
   
   return (
     <Container className="pt-5">
       <div>
-        <h1 className="poppins-bold">Hello, Sa'di is here</h1>
+        <h1 className="poppins-bold text-glitch">Hello, Sa'di is here</h1>
         <div id="job" style={{width: `${jobs[job].length*9.5 + 65}px`}}>
           <h4 className="typewriter">{jobs[job]}</h4>
         </div>
@@ -34,8 +47,8 @@ const Home = () => {
         See more
         <i className="bi bi-chevron-right ms-1"></i>
       </Button>
-      <Card className="p-3">
-        <h1 className="poppins-semibold">Education</h1>
+      <Card className="p-4">
+        <h1 className="poppins-semibold text-glitch">Education</h1>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
@@ -46,6 +59,18 @@ const Home = () => {
           culpa qui officia deserunt mollit anim id est laborum.
         </p>
       </Card>
+      <div className="mt-5">
+        <h1 className="poppins-semibold text-glitch">Projects</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
     </Container>
   );
 };
--- admin/src/pages/landingPage/index.js
+++ admin/src/pages/landingPage/index.js
@@ -1,38 +1,64 @@
 import React, { useEffect, useState } from "react";
-import './index.css'
+import "./index.css";
 import { useDispatch, useSelector } from "react-redux";
-import { Button, Card, Collapse, Container, Nav, NavItem, Navbar, NavbarBrand, NavbarToggler } from "reactstrap";
+import {
+  Button,
+  Card,
+  Collapse,
+  Container,
+  Nav,
+  NavItem,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+} from "reactstrap";
 import { landingTab } from "../../actions/landing";
 import Dashboard from "../dashboard";
 import Model from "./components/model";
 import Home from "../home";
 
 const LandingPage = () => {
-	const tab = useSelector(state => state.landingTab);
-	const dispatch = useDispatch();
+  const tab = useSelector((state) => state.landingTab);
+  const dispatch = useDispatch();
 
-	const [collapsed, setCollapsed] = useState(true);
-	const toggleNavbar = () => setCollapsed(!collapsed);
-	
-	useEffect(() => {
-		dispatch(landingTab("main"))
-	},[]);
+  const [collapsed, setCollapsed] = useState(true);
+  const toggleNavbar = () => setCollapsed(!collapsed);
+
+  useEffect(() => {
+    dispatch(landingTab("main"));
+  }, []);
 
   return (
-	<div>
-		<Navbar faded className="px-3" style={{borderBottom: "1px solid #cccccc"}}>
-			<NavbarBrand>SadiMuslihuddin</NavbarBrand>
-			<NavbarToggler onClick={toggleNavbar} />
-			<Collapse isOpen={!collapsed} navbar>
-				<Nav navbar>
-					<NavItem>About</NavItem>
-				</Nav>
-			</Collapse>
-		</Navbar>
-		{ tab === "main"  && <Home/>}
-		{ tab === "table" && <Dashboard/>}
-		<Model/>
-	</div>
+    <div>
+      <Navbar
+        faded
+        className="px-3"
+        style={{ borderBottom: "1px solid #cccccc" }}
+      >
+        <NavbarBrand>Muslihuddin Sa'di Subhan</NavbarBrand>
+        <NavbarToggler onClick={toggleNavbar} />
+        <Collapse isOpen={!collapsed} navbar>
+          <Nav navbar>
+            <NavItem>About</NavItem>
+            <NavItem>Project</NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+      {tab === "main" && <Home />}
+      {tab === "table" && <Dashboard />}
+      <Model />
+      <div className="p-5" style={{ height: "200px", background: "black" }}>
+		<h5>Contact:</h5>
+        <h5 className="d-flex">
+          <i className="bi bi-envelope me-2"></i>
+          muslihuddinsadi@gmail.com
+        </h5>
+        <h5 className="d-flex">
+          <i className="bi bi-phone-fill me-2"></i>
+          +62 812 6162 2942
+        </h5>
+      </div>
+    </div>
   );
 };
 
