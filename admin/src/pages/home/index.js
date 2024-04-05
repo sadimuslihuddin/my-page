1295e0aa51b1e3baa3ebf7547f6f2531a7c6b857
unknown <muslihuddinsadi@gmail.com>
2024-04-06 02:44:41 +0700

    improve

--- admin/src/pages/home/index.js
+++ admin/src/pages/home/index.js
@@ -3,24 +3,8 @@ import React, { useEffect, useState } from "react";
 import { Button, Card, Col, Container, Row } from "reactstrap";
 import "./index.css";
 
-const jobs = ["Web Developer", "Math modeller", "Android Developer"];
-
 const Home = () => {
-  const [job, setJob] = useState(0);
   const [project, setProject] = useState(1);
-
-  //typing effect
-  useEffect(() => {
-    const interval = setInterval(() => {
-      if(job !== jobs.length-1){
-        setJob(job + 1);
-      } else {
-        setJob(0);
-      }
-    }, 3500);
-
-    return () => clearInterval(interval);
-  }, [job]);
 
   //glitch effect
   useEffect(() => {
@@ -49,9 +33,6 @@ const Home = () => {
     <Container className="pt-5">
       <div>
         <h1 className="poppins-bold text-glitch">Hello, Sa'di is here</h1>
-        <div id="job" style={{width: `${jobs[job].length*9.5 + 65}px`}}>
-          <h4 className="typewriter" style={{color: "#101820"}}>{jobs[job]}</h4>
-        </div>
         <p>
           I'm Fullstack Developer based in Bandung, and i'm very passionate and
           dedicate to my work.
@@ -64,13 +45,7 @@ const Home = () => {
       <Card className="p-4 light-rose">
         <h1 className="poppins-semibold text-glitch">Education</h1>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+            Undergraduate Student From Bandung Institute of Technology.
         </p>
       </Card>
       <div className="mt-5">
--- admin/src/pages/landingPage/index.js
+++ admin/src/pages/landingPage/index.js
@@ -24,24 +24,42 @@ const LandingPage = () => {
         <h1 className="poppins-bold text-center mt-4">Portofolio</h1>
       </Col>
       <Col xs="8" className="p-0">
-        <div
-          faded
-          className="navbar px-3 py-4 w-100"
-        >
-          <div className="d-flex justify-content-around mx-auto" style={{width: "70%"}}>
-            <div className="active">Home</div>
-            <div>About</div>
-            <div>Project & Works</div>
-            <div>Contact</div>
+        <div faded className="navbar px-3 py-4 w-100">
+          <div
+            className="d-flex justify-content-around mx-auto"
+            style={{ width: "70%" }}
+          >
+            <div className="active poppins-semibold">Home</div>
+            <div className="poppins-semibold">About</div>
+            <div className="poppins-semibold">Project & Works</div>
+            <div className="poppins-semibold">Contact</div>
           </div>
         </div>
-        <img className="w-100 profile-img" src="/image/profile-1.jpg">
-		</img>
+        <img className="w-100 profile-img" src="/image/profile-1.jpg"></img>
+        <div className="cover-profile"></div>
       </Col>
-	  <Col xs="12" className="p-0">
-	  	{tab === "main" && <Home />}
+      <Col xs="12">
+        <div className="name-profile">
+          <h1 className="poppins-bold text-glitch">Hello,</h1>
+          <h2 className="poppins-bold text-glitch">
+            Muslihuddin Sa'di is here.
+          </h2>
+          <div className="light-rose mt-3 role-profile">
+            <h4 className="poppins-bold d-inline px-3">I'M</h4>
+            <div>
+              <h4 className="poppins-bold px-3 mb-0">A WEB DEVELOPER</h4>
+              <h4 className="poppins-bold px-3 mb-0">A MATH MODELLER</h4>
+			  <h4 className="poppins-bold px-3 mb-0">AN ANDROID DEVELOPER</h4>
+            </div>
+          </div>
+        </div>
+      </Col>
+      <Col xs="12" className="p-0">
+        {tab === "main" && <Home />}
         {tab === "table" && <Dashboard />}
-        <div className="p-5 mt-5" style={{ height: "200px", background: "black" }}>
+        <div
+          className="footer p-4 mt-5"
+        >
           <h5>Contact:</h5>
           <h5 className="d-flex">
             <i className="bi bi-envelope me-2"></i>
@@ -52,7 +70,7 @@ const LandingPage = () => {
             +62 812 6162 2942
           </h5>
         </div>
-	  </Col>
+      </Col>
     </Row>
   );
 };
